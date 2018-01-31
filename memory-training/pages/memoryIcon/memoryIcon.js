7ecf29f4a058718be0c811587383f3f3f6de4814
orange <326242499@qq.com>
2018-01-31 23:13:51 +0800

    update 记忆图标

--- memory-training/pages/memoryIcon/memoryIcon.js
+++ memory-training/pages/memoryIcon/memoryIcon.js
@@ -10,13 +10,13 @@ Page({
     baseList: [],
     iconList: [],  //全部图标数组
     level: 1, // 当前关卡数
-    pass: [] // 已使用过的图标数组
+    passArr: [], // 已使用过的图标数组
+    currentArr: []// 当前关卡的数组
   },
   
   onLoad: function () {
     this.initIcon();
     this.createCurrent();
-    this.renderIcon();
   },
   // 初始化图标
   initIcon() {
@@ -39,35 +39,40 @@ Page({
   createCurrent() {
     let level = this.data.level;
     let iconList = this.data.iconList;
-    let pass = this.data.pass; 
+    // let passArr = this.data.passArr;
+    let currentArr = this.data.currentArr;
+
+    // let passArr = utils.deepCopy(currentArr); // 将当前icon列表深拷贝后赋值给passArr--- 该方法不可行
+    const [...passArr] = currentArr;
+
+    console.log("passArr:", passArr);
 
     let {index, item} = utils.getOneArray(iconList); // 取出当前这一关的新图标
     console.log('index:', index, 'item:', item);
 
-    pass.push(item);
-    this.data.iconList.splice(index, 1);
+    currentArr.push(item);
+    this.data.iconList.splice(index, 1); // 剔除当前关卡需要展示的图标
     
     this.setData({
-      pass: pass,
+      passArr: passArr,
+      currentArr: currentArr,
       iconList: iconList
+    }, () => {
+      this.renderIcon();
     });
-
-
-
   },
 
   // 将获得当前新数组渲染在页面上
   renderIcon() {
-    console.log("pass:", this.data.pass);
-    console.log("iconList:", this.data.iconList);
-    let baseList = this.data.baseList;
-    let pass = this.data.pass;
 
-    for(let i=0; i<pass.length; i++) {
+    let baseList = this.data.baseList; // 基础数据
+    let currentArr = this.data.currentArr; // 当前关卡所需要的图标
+
+    for(let i=0; i<currentArr.length; i++) {
       for(let j=0; j<baseList.length; j++) {
-        if(pass[i].no === baseList[j].no) {
+        if(currentArr[i].no === baseList[j].no) {
           console.log(baseList[j].index);
-          baseList[j].icon = 'icon-' + baseList[j].icon;
+          baseList[j].icon = baseList[j].no;
         }
       }
     }
@@ -85,8 +90,26 @@ Page({
 
   // 点击图标
   selectIcon(e) {
-    var icon = e.currentTarget.dataset.icon;
-    console.log(icon);
-    
-  }
+    let icon = e.currentTarget.dataset.icon;
+    let passArr = this.data.passArr;
+    console.log("点击到的图标：", icon);
+    console.log("已通过的数组：", passArr);
+    let flag = false;
+    if(icon) {
+      if(passArr.length) {
+        flag = this.data.passArr.every((element, index) => {
+          return element.no === icon;
+        });  
+      }
+
+      console.log(flag);
+      if(!flag) {
+        console.log('点击正确');
+        this.createCurrent();
+      } else {
+        console.log('点击失败');
+      }
+    }
+  },
+ 
 })
--- memory-training/utils/util.js
+++ memory-training/utils/util.js
@@ -38,10 +38,24 @@ const getNumArray = (items, count) => {
   return shuffled.slice(min);
 }
 
+// 复杂数据类型深拷贝，只能深拷贝对象
+const deepCopy = (obj, c) => {
+  var c = c || {};
+  for(var k in obj){
+    if(typeof obj[k] === 'object'){  // 复杂类型数据
+      c[k] = (obj[k].constructor === Array) ? [] : {};
+      deepCopy(obj[k],c[k]);
+    }else{    //简单类型数据
+      c[k] = obj[k];
+    }
+  }
+  return c;
+}
 
 
 module.exports = {
   formatTime: formatTime,
   getOneArray: getOneArray,
   getNumArray: getNumArray,
+  deepCopy: deepCopy,
 }
